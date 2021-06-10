d772612a2d4513ceb988cd92809a3a4f0c447876
Wallin, Petter <petter.z.wallin@teliacompany.com>
2021-06-10 16:32:11 +0200

    link to booking

--- vac.js
+++ vac.js
@@ -6,33 +6,39 @@ const $ = require( "jquery" )( window );
 function parse(data,clinique) {
     $(data).each(function (i, d) {
         $(d.slots).each(function (i, da) {
-
             if (da.available == true) {
-                console.log("Found a time at: "+ clinique+" "+new Date().toLocaleTimeString());
+                console.log("Found a time at: "+ clinique.name+" "+new Date().toLocaleTimeString());
                 console.log(da.when);
                 console.log(d.date)
+                console.log("Boka här: "+ 'https://bokning.mittvaccin.se/klinik/'+clinique.cliniqueId+'/bokning');
             }
         });
     });
 }
 
-var cliniques = new Map;
-cliniques.set('Nacksta', ['https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210607-210613','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210621-210627']);
-cliniques.set('Timra', ['https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210607-210613', 'https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210621-210627']);
-cliniques.set('Harnosand', ['https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210607-210613', 'https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210614-210620', 'https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210621-210627']);
-cliniques.set('kramfors', ['https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210607-210613','https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210614-210620', 'https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210621-210627']);
+let baseUrl = 'https://booking-api.mittvaccin.se/clinique/'
+let clinique1 = {'name': 'Nacksta', 'cliniqueId': '1291', 'appointmentType': '8853'};
+let clinique2 = {'name': 'Timrå', 'cliniqueId': '1311', 'appointmentType': '10620'};
+let clinique3 = {'name': 'Härnösand', 'cliniqueId': '1309', 'appointmentType': '12044'};
+
+let cliniques = [clinique1,clinique2,clinique3];
+ var periods = ['210607-210613', '210614-210620', '210621-210627']
+//cliniques.set('Nacksta', [baseUrl+'1291/appointments/8853/slots/210607-210613','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210621-210627']);
+//cliniques.set('Timra', ['https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210607-210613', 'https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210621-210627']);
+//cliniques.set('Harnosand', ['https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210607-210613', 'https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210614-210620', 'https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210621-210627']);
+//cliniques.set('kramfors', ['https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210607-210613','https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210614-210620', 'https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210621-210627']);
 
 function execute() {
-    cliniques.forEach(function (values, cliniquename) {
+    cliniques.forEach(function (clinique, i) {
+        periods.forEach(function (p, i) {
+            let url = baseUrl+clinique.cliniqueId+'/appointments/'+clinique.appointmentType+'/slots/'+p;
 
-        values.forEach(function (url, i) {
-            //console.log(url);
-            $.get(url,
-                function (data, textStatus, jqXHR) {  // success callback
-                    parse(data, cliniquename);
-                });
+            $.get(url, function (data, textStatus, jqXHR) {  // success callback
+                parse(data, clinique);
+            });
         });
     });
+
 }
 
 setInterval(function() {
@@ -43,4 +49,4 @@ setInterval(function() {
 //RUN first
 execute();
 
-console.log("Started");
+console.log("Running..");
