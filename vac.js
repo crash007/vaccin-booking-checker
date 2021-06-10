39bb80d6cf06129d5998f902dd94e1389920b9ba
Wallin, Petter <petter.z.wallin@teliacompany.com>
2021-06-10 16:37:26 +0200

    added kramfors

--- vac.js
+++ vac.js
@@ -20,8 +20,9 @@ let baseUrl = 'https://booking-api.mittvaccin.se/clinique/'
 let clinique1 = {'name': 'Nacksta', 'cliniqueId': '1291', 'appointmentType': '8853'};
 let clinique2 = {'name': 'Timrå', 'cliniqueId': '1311', 'appointmentType': '10620'};
 let clinique3 = {'name': 'Härnösand', 'cliniqueId': '1309', 'appointmentType': '12044'};
+let clinique4 = {'name': 'kramfors', 'cliniqueId': '1323', 'appointmentType': '9132'};
 
-let cliniques = [clinique1,clinique2,clinique3];
+let cliniques = [clinique1,clinique2,clinique3,clinique4];
  var periods = ['210607-210613', '210614-210620', '210621-210627']
 //cliniques.set('Nacksta', [baseUrl+'1291/appointments/8853/slots/210607-210613','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210621-210627']);
 //cliniques.set('Timra', ['https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210607-210613', 'https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210621-210627']);
